0a6b26ca473fbab2e422a5f0ec257fe08614317a
cromero <andrestrue@hotmail.com>
2016-05-28 01:38:24 -0500

    Administracion - tdocumentos, estregistro, departamentos y municipios

--- administracion/admin.py
+++ administracion/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Post
+from .models import Estregistro
+from .models import Tdocumento
+from .models import Departamento
+from .models import Municipio
 
-admin.site.register(administracion)
+admin.site.register(Estregistro)
+admin.site.register(Tdocumento)
+admin.site.register(Departamento)
+admin.site.register(Municipio)
--- administracion/models.py
+++ administracion/models.py
@@ -5,15 +5,23 @@ from django.db import models
 # Create your models here.
 class Estregistro(models.Model):
 	nombre  = models.CharField(max_length = 10, default='normal')
+	def __unicode__(self): # __unicode__ en Python 2
+		return	self.nombre
 
-class Tdocumentos(models.Model):
+class Tdocumento(models.Model):
 	nombre = models.CharField(max_length=50)
 	sigla = models.CharField(max_length=5)
 	estadoregistro = models.ForeignKey(Estregistro, related_name='Estado')
+	def __unicode__(self): # __unicode__ en Python 2
+		return	self.nombre
 
-class Departamentos (models.Model):
+class Departamento (models.Model):
 	nombre= models.CharField(max_length=50)
+	def __unicode__(self): # __unicode__ en Python 2
+		return	self.nombre
 
-class Municipios (models.Model):
-	departamento = models.ForeignKey(Departamentos, related_name='Departamento')
+class Municipio (models.Model):
+	departamento = models.ForeignKey(Departamento, related_name='Departamento')
 	nombre= models.CharField(max_length=50)
+	def __unicode__(self): # __unicode__ en Python 2
+		return	self.nombre
